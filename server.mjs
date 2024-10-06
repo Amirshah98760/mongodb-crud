6c1a41f5d8b4dd91682cfc85cb20567689b9186d
Amirshah98760 <amirshah98760@gmail.com>
2024-10-06 11:59:44 +0500

    Set up MongoDB connection

--- server.mjs
+++ server.mjs
@@ -2,3 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// MongoDB connection
+mongoose.connect('YOUR_MONGODB_CONNECTION_STRING', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
