32aa723182fe366342cf7c0fad144a6788bdbc2b
Amirshah98760 <amirshah98760@gmail.com>
2024-10-06 12:04:09 +0500

    Create Mongoose schema

--- server.mjs
+++ server.mjs
@@ -17,3 +17,13 @@ mongoose.connect('YOUR_MONGODB_CONNECTION_STRING', {
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
+
+
+// Define a Mongoose schema and model
+const itemSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    gmail: { type: String, required: true, unique: true }, 
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
