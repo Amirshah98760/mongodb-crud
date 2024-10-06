a32eaf92cec771917284f1aa61ac6bd2149642dc
Amirshah98760 <amirshah98760@gmail.com>
2024-10-06 12:13:44 +0500

    Implement Update operation and gitignore file

--- server.mjs
+++ server.mjs
@@ -49,3 +49,13 @@ app.get('/items', async (req, res) => {
     }
 });
 
+
+// Update
+app.put('/items/:id', async (req, res) => {
+    try {
+        const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedItem);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
